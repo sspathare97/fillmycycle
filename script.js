9d91c99c6da0e8ad2b553cc5df0086c1066533fd
sspathare97 <sspathare97@gmail.com>
2017-10-21 21:30:49 +0530

    Update

--- script.js
+++ script.js
@@ -1,37 +1,32 @@
-var tbl,pmarks;
-function count(action){
-	var xmlhttp = new XMLHttpRequest();
-		xmlhttp.onreadystatechange = function() {
-			if (this.readyState == 4 && this.status == 200) {
-				document.getElementById("count").innerHTML = this.responseText;
-			}
-		};
-		xmlhttp.open("POST", "count.php", true);
-  		xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-		xmlhttp.send("action="+action);
-}
-function fetchcount(){
-	count("select");
-	setTimeout(fetchcount, 5000);
-}
-function check(){
-	var marks=parseFloat(document.getElementById("ip").value);
-	if(marks!="" && marks!=pmarks && marks>=20 && marks<=100) {
+function calculate(tbl,ip){
+	var marks=parseFloat(ip.value);
+	if(ip.value.length==2 && ip.value!="10" && marks<=20){
+		ip.value=marks=20;
+	}
+	if(marks>100){
+		ip.value=marks=100;
+	}
+	if(marks>=20 && marks<=100){
 		if(marks%2!=0) {
-			if(marks%2<1) marks=marks-marks%2;
-			else marks+=(2-marks%2);
-			document.getElementById("ip").value=marks;
-			document.getElementById("marks").innerHTML=marks;
-			$("#round").modal();
+			if(marks%2<1){
+				marks=marks-marks%2;
+			}
+			else{
+				marks+=(2-marks%2);
+			}
+			ip.value=marks;
 		}
 		var diff=0;
 		var l=[5,5,5,5,5];
+		var w=[6,6,2,2,4];
 		var lvl=(marks-marks%20)/20+Math.ceil(marks%20/20);
 		l[0]=l[1]=l[2]=l[3]=l[4]=lvl;
-		if(marks%20!=0)
+		if(marks%20!=0){
 			diff=20-marks%20;
-		else
+		}
+		else{
 			diff=0;
+		}
 		while(diff){
 			if(diff>=6){
 				l[1]--;
@@ -56,18 +51,297 @@ function check(){
 		}
 		for(var i=0;i<5;i++) for(var j=1;j<6;j++)
 			tbl.children[i].children[j].innerHTML="";
-		var img='<img src="check.png">';
-		for(var i=0;i<5;i++)
-		tbl.children[i].children[6-l[i] ].innerHTML=img;
-		pmarks=marks;
-		count("update");
+		for(var i=0;i<5;i++){
+			tbl.children[i].children[6-l[i] ].innerHTML='<img src="check.png">';
+			w[i]*=l[i];
+		}
+		w.push(marks);
+		return w;
 	}
-	else if(marks>=20 && marks<=100){
-		$('form').submit();
+	return false;
+}
+function check(){
+	var tbl1=document.getElementById("tbl1");
+	var tbl2=document.getElementById("tbl2");
+	var tbl3=document.getElementById("tbl3");
+	var ip1=document.getElementById("ip1");
+	var ip2=document.getElementById("ip2");
+	if(event.keyCode==9 || event.keyCode==16)
+		return false;
+	var ip = event.target || event.srcElement;
+	if(ip.value){
+		if(ip.value.length>=2 && ip.value!="10"){
+			if(ip==ip1){
+				$(ip2).focus();
+			}
+			else if(ip==ip2){
+				$(ip).blur();
+				location="#result";
+			}
+		}
+	}
+	var w=[];
+	w[2]=[],w[3]=[];
+	if(ip1.value){
+		w[0]=calculate(tbl1,ip1);
+	}
+	if(ip2.value){
+		w[1]=calculate(tbl2,ip2);
+	}
+	if(w[0]){
+		for(var i=0;i<6;i++){
+			tbl3.children[0].children[1+i].innerText=w[0][i];
+		}
+	}
+	else{
+		for(var i=0;i<5;i++) for(var j=1;j<6;j++)
+			tbl1.children[i].children[j].innerHTML="";
+		for(var i=0;i<6;i++){
+			tbl3.children[0].children[1+i].innerText="";
+		}
+	}
+	if(w[1]){
+		for(var i=0;i<6;i++){
+			tbl3.children[1].children[1+i].innerText=w[1][i];
+		}
+	}
+	else{
+		for(var i=0;i<5;i++) for(var j=1;j<6;j++)
+			tbl2.children[i].children[j].innerHTML="";
+		for(var i=0;i<6;i++){
+			tbl3.children[1].children[1+i].innerText="";
+		}
+	}
+	if(w[0] && w[1]){
+		for(var i=0;i<6;i++){
+			w[2][i]=w[0][i]+w[1][i];
+			w[3][i]=w[2][i]/2;
+			tbl3.children[2].children[1+i].innerText=w[2][i];
+			tbl3.children[3].children[1+i].innerText=w[3][i];
+		}
+	}
+	else{
+		for(var i=0;i<6;i++){
+			tbl3.children[2].children[1+i].innerText="";
+			tbl3.children[3].children[1+i].innerText="";
+		}
 	}
 }
+function avg(tbl){
+	var s=0,n=tbl.children.length;
+	for(var i=0;i<n;i++){
+		if(tbl.children[i].lastElementChild.firstElementChild.value){
+			s+= parseInt(tbl.children[i].lastElementChild.firstElementChild.value);
+		}
+		else{
+			return false;
+		}
+	}
+	s/=n;
+	if(s<20) s=20;
+	if(s%2!=0) {
+		if(s%2<1){
+			s=s-s%2;
+		}
+		else{
+			s+=(2-s%2);
+		}
+	}
+	if(tbl==$("#tbl01")[0]){
+		$("#ip1").val(s);
+	}
+	else{
+		$("#ip2").val(s);
+	}
+}
+function perform(){
+	var tbl01=document.getElementById("tbl01");
+	avg(tbl01);
+	var tbl02=document.getElementById("tbl02");
+	avg(tbl02);
+	check();
+}
+function c11change(){
+	var c10=parseInt($("#c10").text());
+	var c11=parseInt($("#c11").val());
+	if(c11 || c11==0){
+		var tbl01=document.getElementById("tbl01");
+		var tbl02=document.getElementById("tbl02");
+		if(c11<=c10){
+			c11=c10;
+			$("#c11").val(c11);
+		}
+		var ct = tbl01.children.length;
+		var pc20 = parseInt($("#c20").text());
+		var pc21 = parseInt($("#c21").val());
+		if($("#c21").val()==""){
+			pc21=pc20+$("#tbl02").children().length-1;
+		}
+		var c20 = c11+1;
+		$("#c20").text(c20);
+		if(c11<ct){
+			var diff=ct-c11;
+			while(diff--){
+				$("#tbl02 tr:first").before(tbl01.lastElementChild);
+			}
+		}
+		else if(c11>=ct){
+			if(c11>=pc21){
+				var t=pc21-ct;
+				while(t--){
+					$("#tbl01").append(tbl02.firstElementChild);
+				}
+				$("#tbl02").append('<tr>\
+					<td>'+c20+'</td>\
+					<td><input class="form-control input-sm exp" type="number" min="0" max="100" step="1"/></td>\
+					</tr>');
+				var diff=c11-pc21;
+				var c=pc21+1;
+				while(diff--){
+					$("#tbl01").append('<tr>\
+						<td>'+c+'</td>\
+						<td><input class="form-control input-sm exp" type="number" min="0" max="100" step="1"/></td>\
+						</tr>');
+					c++;
+				}
+			}
+			else{
+				var diff=c11-ct;
+				while(diff--){
+					$("#tbl01").append(tbl02.firstElementChild);
+				}
+			}
+			$("input.exp").on("keyup", expkeyup);
+			$("input.exp").on("change", perform);
+		}
+
+		perform();
+	}
+}
+function c21change(){
+	var c21=parseInt($("#c21").val());
+	if(c21 || c21==0){
+		var tbl02=document.getElementById("tbl02");
+		var c20=parseInt($("#c20").text());
+		var c21=parseInt($("#c21").val());
+		if(c21<=c20){
+			c21=c20;
+			$("#c21").val(c21);
+		}
+		var tot=c21-c20+1;
+		var act=tbl02.children.length;
+		var diff= tot-act;
+		if(diff<0){
+			diff*=-1;
+			while(diff--){
+				tbl02.removeChild(tbl02.lastElementChild);
+			}
+		}
+		else{
+			var ct=c20+tbl02.children.length;
+			while(diff--){
+				$("#tbl02").append('<tr>\
+					<td>'+ct+'</td>\
+					<td><input class="form-control input-sm exp" type="number" min="0" max="100" step="1"/></td>\
+					</tr>');
+				ct++;
+			}
+			$("input.exp").on("keyup", expkeyup);
+			$("input.exp").on("change", perform);
+		}
+
+		perform();
+
+	}
+}
+function expkeyup(){
+	if(event.keyCode==9 || event.keyCode==16)
+		return false;
+	var ip = event.target || event.srcElement;
+	if(ip.value){
+		if(ip.value.length>=2 && ip.value!="10"){
+			var next=ip.parentElement.parentElement.nextElementSibling
+			if(next){
+				next=next.lastElementChild.firstElementChild;
+			}
+			var last1=ip.parentElement.parentElement.parentElement.lastElementChild.lastElementChild.firstElementChild;
+			var last2=document.getElementById("tbl02").lastElementChild.lastElementChild.firstElementChild;
+			var val = parseInt(ip.value);
+			if(val<0){
+				ip.value=0;
+			}
+			else if(val>100){
+				ip.value=100;
+			}
+			if(ip==last2){
+				$(ip).blur();
+				location="#result";
+			}
+			else if(ip==last1){
+				$("#tbl02 tr input:first").focus();			
+			}
+			else{
+				$(next).focus();
+			}
+		}
+	}
+	perform();
+}
 $(document).ready(function(){
-	$('#ip').focus();
-	tbl=document.getElementById("tbl");
-	fetchcount();
+	$("#c11").focus();
+	$("#c11").click();
+	$("#c11").trigger('click');
+	$("#c11").on("keyup",function(){
+		if(event.keyCode==9 || event.keyCode==16 || event.keyCode>=37 && event.keyCode<=40)
+			return false;
+		var ip = event.target || event.srcElement;
+		var c10=parseInt($("#c10").text());
+		var c11=parseInt($("#c11").val());
+		if(c11>15){
+			$("#c11").val(15)
+		}
+		if(c11>c10){
+			c11change();
+		}
+		if(ip.value.length>=1 && ip.value!="1"){
+			$("#c21").focus();
+		}
+	});
+	$("#c11").on("change",c11change);
+	$("#c21").on("keyup",function(){
+		if(event.keyCode==9 || event.keyCode==16 || event.keyCode>=37 && event.keyCode<=40)
+			return false;
+		var ip = event.target || event.srcElement;
+		var c20=parseInt($("#c20").text());
+		var c21=parseInt($("#c21").val());
+		if(c21>30){
+			$("#c21").val(30)
+		}
+		if(c21>=c20){
+			c21change();
+		}
+		if(ip.value.length>=1 && ip.value!="1" && ip.value!="2" && ip.value!="3"){
+			$("#tbl01 tr input:first").focus();
+		}
+	});
+	$("#c21").on("change",c21change);
+	$("input.exp").on("keyup", expkeyup);
+	$("input.exp").on("change", perform);
+	$("input.ip").on("keyup", check);
+	$("input.ip").on("change", check);
+	$("button.reset").on("click", function(){
+		perform();
+		var tbl1=document.getElementById("tbl1");
+		var tbl2=document.getElementById("tbl2");
+		var tbl3=document.getElementById("tbl3");
+		for(var i=0;i<5;i++) for(var j=1;j<6;j++)
+			tbl1.children[i].children[j].innerHTML="";
+		for(var i=0;i<5;i++) for(var j=1;j<6;j++)
+			tbl2.children[i].children[j].innerHTML="";
+
+		for(var j=0;j<4;j++) for(var i=0;i<6;i++){
+			tbl3.children[j].children[1+i].innerText="";
+		}
+		location="#";
+	});
 });
--- table.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE `count` (
-  `count` bigint(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
-INSERT INTO `count` (`count`) VALUES (0);
